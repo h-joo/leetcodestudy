a8c4a9b07e562acb33de7c1d6c83e5b31d24b605
Hana Joo <izazu1725@gmail.com>
2023-07-28 23:02:23 +0200

    Remove all Solutions

--- study0/solutions/week3/climbing_stairs.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        distinct_ways = [0, 1, 2, 3]    
-        for i in range(4, n + 1):
-            distinct_ways.append(distinct_ways[i - 1] + distinct_ways[i - 2])
-        
-        return distinct_ways[n]
-
--- study0/solutions/week5/group_anagrams.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grouped_anagrams = defaultdict(list)
-        for string in strs:
-            grouped_anagrams["".join(sorted(string))].append(string)
-        
-        return grouped_anagrams.value()
