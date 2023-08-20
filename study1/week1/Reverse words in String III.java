d356b5d6a170f5e78ad06bf38e3bc84b500fe2a0
Haun <105895293+Rhaun95@users.noreply.github.com>
2023-08-20 22:46:49 +0200

    Update Reverse words in String III.java

--- study1/week1/Reverse words in String III.java
+++ study1/week1/Reverse words in String III.java
@@ -1,13 +1,16 @@
 class Reverse words in String III {
+   
     public String reverseWords(String s) {
-        String res = "";
-       
-        for(String a : s.split(" ")){
-            StringBuffer sb = new StringBuffer(a);
-            String reverse = sb.reverse().toString();
-            res += reverse + " ";
+        String res = "";                                // space complexity O(1)
+      
+        for(String a : s.split(" ")){                   // linear time complexity O(n), n is the length of the input String  
+            StringBuffer sb = new StringBuffer(a);      // space complexity O(m), m is the length of a particular word
+            String reverse = sb.reverse().toString();   // linear time complexity O(m)  
+            res += reverse + " ";                       // constant time complexity O(1)
         }
-        res = res.stripTrailing();
-        return res;
+        res = res.stripTrailing();                      // constant time complexity O(1)  
+        return res;                                     // space complexity O(n)
     }
+// Time Complexity = O(n*m)
+// Space Complexity = O(n+m) = O(n)
 }
