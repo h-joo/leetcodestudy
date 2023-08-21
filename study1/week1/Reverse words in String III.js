bc359ae67025633d879e1e06241d7b6f07198288
Seung-Jae Yoon <casysj@hotmail.com>
2023-08-22 00:17:34 +0200

    주석추가

--- study1/week1/Reverse words in String III.js
+++ study1/week1/Reverse words in String III.js
@@ -5,6 +5,10 @@
 
 //  떠오른데로 풀기
 //  O(n^2)
+/**
+ *  for문 안에 for문을 넣어서 돌려서 n*n형태의 연산이 이루어진다
+ *  때문에 O(n^2)
+ */
 var reverseWords = function(s) {
     let returnVal ='';
     const tmp = s.split(" ");
@@ -29,6 +33,10 @@ reverseWords('hello world man! zahooyy');
 
 //  다른방법으로 풀기
 //  O(n^2)
+/**
+ *  for문이 있고 그 안에 돌아가는 reverseString이 재귀형태로 작동해서
+ *  이것도 마찬가지로 O(n^2) 이다
+ */
 var reverseWords = function(s) {
     let returnVal ='';
     const wordsArr = s.split(" ");
