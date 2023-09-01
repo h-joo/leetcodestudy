45ce69d40f68568271b3fefd6e4c9e561a6b5312
Jeongwon Na <jeongwon.na@tum.de>
2023-09-01 23:53:08 +0200

    Update n-th-tribonacci-number.java
    updated version considering feedback which uses the array with length 4

--- study1/week2/n-th-tribonacci-number.java
+++ study1/week2/n-th-tribonacci-number.java
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/n-th-tribonacci-number/
 class Solution {
+    // updated version considering feedback which uses the array with length 4
     // Time Complexity: O(n)
-    // Space Complexity: O(n)
+    // Space Complexity: O(1)
     public int tribonacci(int n) {
         if (n == 0) {
             return 0;
@@ -9,15 +10,38 @@ class Solution {
             return 1;
         }
 
-        int[] triboArr = new int[n+1];  // using dynamic programming
+        int[] triboArr = new int[4];  // using dynamic programming
         triboArr[0] = 0;
         triboArr[1] = 1;
         triboArr[2] = 1;
 
         for (int i = 3; i <= n; ++i) {
-            triboArr[i] = triboArr[i-3] + triboArr[i-2] + triboArr[i-1];
+            triboArr[3] = triboArr[0] + triboArr[1] + triboArr[2];
+            triboArr[0] = triboArr[1];
+            triboArr[1] = triboArr[2];
+            triboArr[2] = triboArr[3];
         }
 
-        return triboArr[n];
+        return triboArr[3];
     }
+
+//    // old version
+//    public int tribonacci(int n) {
+//        if (n == 0) {
+//            return 0;
+//        } else if (n == 1 || n == 2) {
+//            return 1;
+//        }
+//
+//        int[] triboArr = new int[n+1];  // using dynamic programming
+//        triboArr[0] = 0;
+//        triboArr[1] = 1;
+//        triboArr[2] = 1;
+//
+//        for (int i = 3; i <= n; ++i) {
+//            triboArr[i] = triboArr[i-3] + triboArr[i-2] + triboArr[i-1];
+//        }
+//
+//        return triboArr[n];
+//    }
 }
