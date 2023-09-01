03a0da94959421a47848dec9e69a1a77df92ab4b
Jeongwon Na <jeongwon.na@tum.de>
2023-09-01 23:58:38 +0200

    Update longest-palindrome.java
    update considering feedback

--- study1/week2/longest-palindrome.java
+++ study1/week2/longest-palindrome.java
@@ -3,33 +3,26 @@ class Solution {
     // Time Complexity: O(n) -> 2 loop
     // Space Complexity: O(n) becausde of hashmap and char array
     public int longestPalindrome(String s) {
-        char[] charArr = s.toCharArray();
         HashMap<Character, Integer> letterMap = new HashMap<>();
-        int palindrome = 0;
-        boolean isOne = false;
 
         // put key and value into letterMap
-        for (char ch : charArr) {
-            if (letterMap.containsKey(ch)) {
-                letterMap.put(ch, letterMap.get(ch)+1);
-            } else {
-                letterMap.put(ch, 1);
-            }
+        for (int i = 0 ;i < s.length() ; ++i) {
+            char ch = s.charAt(i);
+            letterMap.put(ch, letterMap.getOrDefault(ch, 0) + 1);
         }
 
-        if (letterMap.keySet().size() == 1) {   // if only one letter exists
-            return letterMap.get(charArr[0]);
-        }
-
+        boolean isOne = false;
+        int palindrome = 0;
         for (Character ch : letterMap.keySet()) {
-            if (letterMap.get(ch) % 2 == 0) { // the num of letter is even number
-                palindrome += letterMap.get(ch);
+            int count = letterMap.get(ch);
+            if ( count % 2 == 0) { // the num of letter is even number
+                palindrome += count;
             } else {    // the num of letter is odd number
                 if (!isOne) {   // only one character which counts as odd number is permitted
                     palindrome += 1;
                     isOne = true;
                 }
-                palindrome += (letterMap.get(ch)-1);
+                palindrome += count-1;
             }
         }
 
