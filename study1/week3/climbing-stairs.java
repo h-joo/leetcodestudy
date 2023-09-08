aedc1e5d8ec03c3b89e38d6d13ca2c95e6fefe6a
Jeongwon Na <jeongwon.na@tum.de>
2023-09-08 23:04:24 +0200

    Update climbing-stairs.java
    updated, learning from feedback

--- study1/week3/climbing-stairs.java
+++ study1/week3/climbing-stairs.java
@@ -3,22 +3,10 @@
 // Space complexity: O(1) because using only 3-size-array
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) {   // edge cases
-            return 1;
-        } else if (n == 2) {
-            return 2;
-        }
-
-        int[] temp = new int[3];    // using dp
-        temp[0] = 1;
-        temp[1] = 2;
-
+        int[] temp = new int[]{1, 2};
         for (int i = 2; i < n; ++i) {
-            temp[2] = temp[0] + temp[1];
-            temp[0] = temp[1];
-            temp[1] = temp[2];
+            temp[i % 2] = temp[0] + temp[1];
         }
-
-        return temp[2];
+        return temp[(n - 1) % 2];
     }
 }
