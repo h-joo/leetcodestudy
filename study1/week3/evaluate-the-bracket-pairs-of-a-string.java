2d528ac1430f236935332251cfb081b67e6b77d9
Jeongwon Na <jeongwon.na@tum.de>
2023-09-09 17:07:42 +0200

    Update evaluate-the-bracket-pairs-of-a-string.java

--- study1/week3/evaluate-the-bracket-pairs-of-a-string.java
+++ study1/week3/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,26 +1,27 @@
 // https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string
 // Time Complexity: O(n)
 // Space Complexity: O(n), because of hashmap
+
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         HashMap<String, String> wordMap = new HashMap<>();  // key: word to be changed, value: changing word
 
         for (List<String> word : knowledge) {
-            wordMap.put("(" + word.get(0) + ")", word.get(1));
+            wordMap.put(word.get(0), word.get(1));
         }
 
         for (int i = 0; i < s.length(); ++i) {
             char ch = s.charAt(i);
             if (ch == '(') {    // bracket found
                 int end = s.indexOf(")", i);
-                result += wordMap.getOrDefault(s.substring(i, end+1), "?");
+                result.append(wordMap.getOrDefault(s.substring(i+1, end), "?"));
                 i = end;  // change index to the end
             } else {
-                result += ch;
+                result.append(ch);
             }
         }
 
-        return result;
+        return result.toString();
     }
 }
